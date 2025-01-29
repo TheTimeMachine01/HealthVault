cb43639853f75c56464aa0be863eb40ff82b9376
Ashish Verma <129376147+TheTimeMachine01@users.noreply.github.com>
2025-01-29 20:01:49 +0530

    Inital Commit/ Project Creation

---
+++ src/main/java/com/example/healthvault/HealthVaultApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/healthvault/HealthVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
